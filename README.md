bcc8d595e42be5457caa683f19da68dfc098dc33
JNRuan <jnruan@gmail.com>
2021-09-29 20:34:58 +1000

    Converter script from autsl videos to rgb frames. Added custom csv resources with EN labels.

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# auslan-ml-feature-exploration
-Honours Project exploring Auslan features in the context of Machine Learning
+# Auslan ML Feature Exploration
+Honours Project exploring Auslan features in the context of Machine Learning.
+
+## AUTSL videos to rgb frames
+In `data/autsl_frames.py`, script converts AUTSL dataset [1] from mp4 videos to jpg frames 
+for each signer and sample, stored under class label folders.
+
+Run script: python autsl_frames.py 
+-i "path/to/autsl_videos" 
+-o "path/to/autsl/frames" 
+\[-d dataset partition, optional train or val or test, default "train"]
+\[-f optional fps setting, default='30']
+
+e.g., `python autsl_frames.py -i 'path/to/autsl' -o 'path/to/autsl/frames' -d 'test' -f '15'`
+
+Note that autsl should have file structure:
+
+```
+autsl/
+├─ train/
+│  ├─ ...videos.mp4
+│  ├─ ...videos.mp4
+│  ├─ ...videos.mp4
+├─ val/
+├─ test/
+SignList_ClassId_TR_EN.csv
+test_labels.csv
+test_labels_en.csv
+train_labels.csv
+train_labels_en.csv
+validation_labels.csv
+val_labels_en.csv
+```
+
+Where `*_labels_en.csv` are generated csvs of `*_labels.csv` but with an EN column 
+for english class names.
+
+Files provided in `resources` folder, and was created by me for convenience. 
+Ensure these live in the `autsl` dataset folder. Similar can be created for other languages.
+To do so make use of `SignList_ClassId_TR_EN.csv` which comes with AUTSL dataset.
+
+**Output Structure**
+
+```
+frames/
+├─ rgb/
+│  ├─ test/
+│  │  ├─ word1/
+│  │  │  ├─ signer1_sample1_001.jpg
+│  │  │  ├─ signer1_sample1_002.jpg
+│  │  │  ├─ ...
+│  │  │  ├─ signerX_sampleY_nnn.jpg
+│  │  ├─ word2/
+│  │  │  ├─ signerX_sampleY_nnn.jpg
+│  │  │  ├─ ...
+│  │  ├─ ...more_words/
+```
+
+You can then load these images with class labels as folder names.
+
+Due to the number of images, will need to batch load and also deal with sequences.
+Sequences will be linked to each signer and sample as per video name. With class labels/words
+as per class label english words in csvs provided by AUTSL dataset.
+
+## References
+[1] Sincan, O. M., Keles, H. Y. “AUTSL: A Large Scale Multi-modal Turkish Sign Language Dataset and Baseline Methods”. IEEE Access, vol. 8, pp. 181340-181355, 2020.
---
+++ data/autsl_frames.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+"""
+This script extracts AUTSL videos into frames.
+
+Dataset: http://cvml.ankara.edu.tr/datasets/
+
+Note that the dataset image size is 512x512, may require preprocessing to resize images prior to
+consumption or batching.
+
+Run: python autsl_frames.py -i path/to/autsl -o path/to/autsl/frames [-d 'train' or 'val' or 'test']
+e.g., python autsl_farmes.py -i "~/datasets/autsl" -o "~/datasets/autsl/frames" -d "test"
+
+Help: python autsl_frames.py -h
+"""
+# Standard
+from pathlib import Path, PurePath
+
+import argparse
+import glob
+import os
+import subprocess
+
+# Packages
+from tqdm import tqdm
+
+import pandas as pd
+
+################################################################################
+GLOB_PATTERN_RGB = '*_color.mp4'
+
+
+def construct_rgb_dataset(input_path: str, output_path: str, dataset_cat: str, fps: str):
+    """
+    Constructs rgb frame dataset from videos in input_path/dataset_cat. E.g., input_path/train.
+
+    Output path will save frames in output_path/rgb/dataset_cat/{word}/. E.g., output_path/rgb/train/{word}/
+    Each {word} folder will contain frames related to {word}, with video name consistent.
+
+    Args:
+        input_path: Dataset root path containing train, val, test folders.
+        output_path: Output path to save frames.
+        dataset_cat: Dataset category to convert to frames.
+    """
+    # Get all videos from input_path/dataset_cat
+    data_path = PurePath(input_path, dataset_cat)
+    video_paths = glob.glob(f"{PurePath(data_path, GLOB_PATTERN_RGB)}")
+    video_filenames = [os.path.split(p)[1] for p in video_paths]
+    sample_names = ["_".join(f.split("_")[:2]) for f in video_filenames]
+    class_labels = pd.read_csv(PurePath(input_path, f"{dataset_cat}_labels_en.csv"))
+
+    print(f"Found {len(video_paths)} videos in {data_path}")
+
+    # Setup paths
+    out_path = PurePath(output_path, "rgb", dataset_cat)
+    if not Path(out_path).exists():
+        os.makedirs(out_path)
+
+    # Extract frames
+    pbar = tqdm(video_paths)
+    for idx, item in enumerate(pbar):
+        pbar.set_description(f"Processing: {item}")
+        print()
+
+        # Setup class label and path
+        class_label = class_labels.loc[class_labels['Sample'] == sample_names[idx]]['EN'].item()
+        class_label_path = PurePath(out_path, class_label)
+        if not Path(class_label_path).exists():
+            os.makedirs(class_label_path)
+
+        # Setup filename
+        frame_pattern = f"{sample_names[idx]}_%03d.jpg"
+        frame_path = PurePath(class_label_path, frame_pattern)
+
+        # Call ffmpeg subprocess to make frames
+        subprocess.call([
+            'ffmpeg',
+            '-i', item,
+            '-qscale:v', '2',
+            '-vf', f'fps={fps}',
+            '-loglevel', '0',
+            frame_path
+        ], shell=True)
+
+
+def validate_args(args):
+    if not Path(args.input).exists():
+        msg = f"Input {args.input} does not exist"
+        raise ValueError(msg)
+    if not Path(args.input).is_dir():
+        msg = f"Input {args.input} is not a folder."
+        raise ValueError(msg)
+    if not Path(args.output).exists():
+        msg = f"Output {args.output} does not exist"
+        raise ValueError(msg)
+    if not Path(args.output).is_dir():
+        msg = f"Output {args.output} is not a folder."
+        raise ValueError(msg)
+    if args.dataset not in ['train', 'val', 'test']:
+        msg = f"Dataset {args.dataset} not valid, use 'train', 'val', or 'test'"
+        raise ValueError(msg)
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(description="AUTSL Frames Data Creation")
+    arg_parser.add_argument('-i',
+                            '--input',
+                            help='Dataset input root path, e.g., path/to/autsl',
+                            required=True)
+    arg_parser.add_argument('-o',
+                            '--output',
+                            help="Dataset output root path, e.g., path/to/autsl/frames",
+                            required=True)
+    arg_parser.add_argument('-d',
+                            '--dataset',
+                            help='[Optional] Dataset to use, eg., train or val or test, default=train',
+                            default='train')
+    arg_parser.add_argument('-f',
+                            '--fps',
+                            help='[Optional] FPS to extract at, default=30',
+                            default='30')
+    args = arg_parser.parse_args()
+
+    validate_args(args)
+    construct_rgb_dataset(args.input, args.output, args.dataset, args.fps)
+
+
+if __name__ == '__main__':
+    main()
