0fea3048780112e6dcb05b90c283fccde285335d
JNRuan <jnruan@gmail.com>
2021-09-29 20:45:35 +1000

    Added RGB or Depth mode.

--- data/autsl_frames.py
+++ data/autsl_frames.py
@@ -13,6 +13,7 @@ e.g., python autsl_farmes.py -i "~/datasets/autsl" -o "~/datasets/autsl/frames" 
 Help: python autsl_frames.py -h
 """
 # Standard
+from enum import Enum
 from pathlib import Path, PurePath
 
 import argparse
@@ -27,9 +28,15 @@ import pandas as pd
 
 ################################################################################
 GLOB_PATTERN_RGB = '*_color.mp4'
+GLOB_PATTERN_DEPTH = '*_depth.mp4'
 
 
-def construct_rgb_dataset(input_path: str, output_path: str, dataset_cat: str, fps: str):
+class CameraMode(Enum):
+    RGB = 0
+    DEPTH = 1
+
+
+def construct_rgb_dataset(input_path: str, output_path: str, dataset_cat: str, fps: str, mode: str):
     """
     Constructs rgb frame dataset from videos in input_path/dataset_cat. E.g., input_path/train.
 
@@ -40,10 +47,15 @@ def construct_rgb_dataset(input_path: str, output_path: str, dataset_cat: str, f
         input_path: Dataset root path containing train, val, test folders.
         output_path: Output path to save frames.
         dataset_cat: Dataset category to convert to frames.
+        fps: FPS setting for ffmpeg.
+        mode: Camera mode of videos, either RGB or Depth
     """
     # Get all videos from input_path/dataset_cat
     data_path = PurePath(input_path, dataset_cat)
-    video_paths = glob.glob(f"{PurePath(data_path, GLOB_PATTERN_RGB)}")
+    if mode == CameraMode.DEPTH:
+        video_paths = glob.glob(f"{PurePath(data_path, GLOB_PATTERN_DEPTH)}")
+    else:
+        video_paths = glob.glob(f"{PurePath(data_path, GLOB_PATTERN_RGB)}")
     video_filenames = [os.path.split(p)[1] for p in video_paths]
     sample_names = ["_".join(f.split("_")[:2]) for f in video_filenames]
     class_labels = pd.read_csv(PurePath(input_path, f"{dataset_cat}_labels_en.csv"))
@@ -118,10 +130,14 @@ def main():
                             '--fps',
                             help='[Optional] FPS to extract at, default=30',
                             default='30')
+    arg_parser.add_argument('-m',
+                            '--mode',
+                            help='[Optional] Camera mode of videos. rgb=0, depth=1, default=0',
+                            default='0')
     args = arg_parser.parse_args()
 
     validate_args(args)
-    construct_rgb_dataset(args.input, args.output, args.dataset, args.fps)
+    construct_rgb_dataset(args.input, args.output, args.dataset, args.fps, args.mode)
 
 
 if __name__ == '__main__':
